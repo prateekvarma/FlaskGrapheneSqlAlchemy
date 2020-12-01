2c7200e91adc87cf400e66ef0266d691f0b47f36
Prateek Varma <prtk.varma@gmail.com>
2020-12-01 15:19:25 -0600

    added data to SQLite via command line, to run graphiql - fixed some path errors

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask_graphql import GraphQLView
 
-from .models import db_session
-from .schema import schema, Department
+from models import db_session
+from schema import schema, Department
 
 app = Flask(__name__)
 app.debug = True
@@ -12,7 +12,7 @@ app.add_url_rule(
     view_func=GraphQLView.as_view(
         'graphql',
         schema=schema,
-        graphiql=True
+        graphiql=True # for having the GraphiQL interface
     )
 )
 
@@ -20,5 +20,5 @@ app.add_url_rule(
 def shutdown_session(exception=None):
     db_session.remove()
 
-id __name__ == '__main__':
+if __name__ == '__main__':
     app.run()
